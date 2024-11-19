0c286834fb284b6c9adaf49f8b7f7dc363bc21c0
Adesina Joshua <tolujosh1@gmail.com>
2024-11-19 18:29:48 +0100

    refresher on data types

---
+++ variables and data types/variables.py
@@ -0,0 +1,44 @@
+# Variables: are used to store data which can be used later
+# data types include:
+# 1. Strings
+# 2. Integers
+# 3. Booleans
+# 4. Floats
+# 5. Lists
+# 6. Tuples
+# 7. Dictionaries
+
+# 1. Strings
+name = "Joshua"
+country = "United States"
+
+print(name) # Output: Joshua
+print(name) # Output: United States
+
+
+# 2. Integers
+age = 200
+no_of_apples = 25
+
+print(age) # Output: 200
+print(no_of_apples) # Output: 25
+
+
+# 3. Booleans
+is_adult = True
+is_boy = False
+print(is_adult) # Output: True
+print(is_boy) # Output: False
+
+
+# 4. Floats
+age = 24.1
+no_of_apples = 25.99
+print(age) # Output: 24.1
+print(no_of_apples) # Output: 25.99
+
+# 5. Lists are ordered collections that can store multiple items, which may be of different data types.
+list_example = ['joshua', 39, 48.2, True]
+fruits = ["banana", "apple", "cashew"]
+print(list_example) # Output: ['joshua', 39, 48.2, True]
+print(fruits) # Output: ['banana', 'apple', 'cashew']
